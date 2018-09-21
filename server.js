095e9e69375ee57cd5d021310dc8f06c1ece0a1a
Brendon Richards <brendonrichards93@gmail.com>
2018-09-21 06:46:58 +0000

    Install Chatkit SDK and initial server setup

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const Chatkit = require('pusher-chatkit-server');
 
 const app = express();
+
+const chatkit = new Chatkit.default({
+  instanceLocator: 'v1:us1:fbbd66fa-a219-441e-99ea-b173bc7e653a',
+  key: '9ce2ff48-436b-4965-a55c-e40410ac7364:BV8SBZ7VA7U4rszBNjhofaaO4DuOS3M6BR/cxIU+bzY='
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
